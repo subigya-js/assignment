cfbead56d484903faed04a0ea6275cfda37add3b
subigya-js <developer.subigya@gmail.com>
2025-04-05 13:05:17 +0530

    Update FeatureIcon component to display feature title below the icon

--- components/FeatureIcon.tsx
+++ components/FeatureIcon.tsx
@@ -19,8 +19,9 @@ export default function FeatureIcon({
             className={`${className} cursor-pointer group`}
             onClick={() => setClicked(!clicked)}
         >
-        <div className="w-20 h-20 rounded-full shadow-md flex items-center justify-center">
+        <div className="w-20 h-20 rounded-full flex flex-col items-center justify-center gap-2">
             <Image src={iconSrc} alt={feature.title} width={40} height={40} />
+            <p className='text-sm w-[160px] text-center'>{feature.title}</p>
         </div>
 
             {clicked && (
